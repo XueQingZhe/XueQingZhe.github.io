6d6185913f5a043b87ac5b7337aeb8dabdd3cba6
XueQingZhe <87290118+XueQingZhe@users.noreply.github.com>
2025-12-20 14:50:54 +0000

    Deploying to gh-pages from @ XueQingZhe/XueQingZhe.github.io@510cf62fc583a2a057bfe34d1bcca14c8c122698 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/books/";
           },
+        },{id: "nav-tutorials",
+          title: "tutorials",
+          description: "技术分享与教程",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/tutorials/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
